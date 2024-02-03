6559ed6fcc7c74165e05d29c25c5eb52f9c87047
gregoryerrl <gregoryerrl@gmail.com>
2024-02-04 00:28:37 +0800

    password

--- helpers/db_connection.php
+++ helpers/db_connection.php
@@ -2,7 +2,7 @@
 
   $server   = 'localhost';
   $username = 'root';
-  $password = '';
+  $password = 'password';
   $db       = 'tricipay';
   $conn     = new mysqli( $server, $username, $password, $db );
   
